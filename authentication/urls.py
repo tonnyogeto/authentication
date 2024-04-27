9446f7430ea8d0ced564e434e5a6ad634c88d79a
tonnyogeto <tonnymaishaogeto@gmail.com>
2024-04-28 01:16:26 +0300

    url mapping

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path(),
+    path('', views.homepage, name=""),
+    path('register', views.register, name="register"),
+    path('log-in', views.log_in,name="log-in"),
+    path('dashboard', views.dashboard, name="dashboard"),
+
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 
+def homepage(request):
+    # return HttpResponse("This is the homepage") returning httpResponse
+    return render(request, 'authentication/index.html')
+
+def register(request):
+    return render(request, 'authentication/register.html')
+
+def log_in(request):
+    return render(request, 'authentication/my-login.html')
+
+def dashboard(request):
+    return render(request, 'authentication/dashboard.html')
