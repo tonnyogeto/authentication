58ae7a61a4adb68f7f76c4d3faae66f2260099aa
tonnyogeto <tonnymaishaogeto@gmail.com>
2024-05-02 14:58:04 +0300

    login functionality

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,8 +1,18 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+
+from django import forms
+from django.forms.widgets import TextInput, PasswordInput
 
 class CreateUserForm(UserCreationForm):
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class LoginForm(AuthenticationForm):
+
+    username=forms.CharField(widget=TextInput())
+    password=forms.CharField(widget=PasswordInput())
+
